41cbc8e6d8bfae5558df388dd3cb2ce2baeeca22
JrtPec <jan.pecinovsky@student.groept.be>
2014-07-31 17:25:10 +0200

    config

--- config.py
+++ config.py
@@ -8,10 +8,11 @@ FACEBOOK_APP_ID = '251477738393481'
 FACEBOOK_APP_SECRET = '723ce8fda1034eaa465489362cf52b31'
     
 if os.environ.get('DATABASE_URL') is None:
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
 else:
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
 
 # email server
 MAIL_SERVER = 'smtp.gmail.com'
